b468ea8bf08887746260173c6bbb5534e8d011a0
Reinhard <reinhard1994@aol.com>
2015-01-20 13:46:46 +0100

    Graphdarstellung funktioniert noch nicht ...

--- SelfControl/src/de/dresden/es/inf/Selfcontrol/SekActivity.java
+++ SelfControl/src/de/dresden/es/inf/Selfcontrol/SekActivity.java
@@ -1,11 +1,28 @@
 package de.dresden.es.inf.Selfcontrol;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class SekActivity extends Activity{
+	
+//	public void drawGraph(){
+//		
+//		GraphView gView = (GraphView) findViewById(R.id.sekGraph);
+//		
+//		BarGraphSeries<DataPoint> series = new BarGraphSeries<DataPoint>( new DataPoint[]{
+//				new DataPoint(1, 2),
+//				
+//				new DataPoint(2, 4)
+//		}); 
+//		
+//		gView.addSeries(series);
+//	}
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -19,6 +36,9 @@ public class SekActivity extends Activity{
 		
 		sekCounter.setText(count);
 		
+		//drawGraph();
+		
 	}
 
 }
+
