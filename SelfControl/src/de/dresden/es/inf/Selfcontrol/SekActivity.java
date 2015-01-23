8fe3f1050701e25c905ef3902e39ca3be3430cc5
Reinhard <reinhard1994@aol.com>
2015-01-23 17:13:58 +0100

    GraphView funktioniert, solange alle *.jar aus dem /libs-folder im BuildPath importiert werden ;)

--- SelfControl/src/de/dresden/es/inf/Selfcontrol/SekActivity.java
+++ SelfControl/src/de/dresden/es/inf/Selfcontrol/SekActivity.java
@@ -3,6 +3,7 @@ package de.dresden.es.inf.Selfcontrol;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -11,18 +12,30 @@ import android.widget.TextView;
 
 public class SekActivity extends Activity{
 	
-//	public void drawGraph(){
-//		
-//		GraphView gView = (GraphView) findViewById(R.id.sekGraph);
-//		
+	public void drawGraph(){
+		
+		GraphView gView = (GraphView) findViewById(R.id.sekGraph);
+		
 //		BarGraphSeries<DataPoint> series = new BarGraphSeries<DataPoint>( new DataPoint[]{
 //				new DataPoint(1, 2),
 //				
-//				new DataPoint(2, 4)
-//		}); 
+//				new DataPoint(2, 4),
 //		
-//		gView.addSeries(series);
-//	}
+//				
+//		}); 
+		
+		DataPoint[] data = new DataPoint[20];
+		
+		for(int i=0; i<=19;i++){
+			data[i] = new DataPoint(i*2, 2.0d+i);
+		}
+		
+		BarGraphSeries<DataPoint> series = new BarGraphSeries<DataPoint>(data);
+		
+		
+		
+		gView.addSeries(series);
+	}
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -36,7 +49,7 @@ public class SekActivity extends Activity{
 		
 		sekCounter.setText(count);
 		
-		//drawGraph();
+		drawGraph();
 		
 	}
 
