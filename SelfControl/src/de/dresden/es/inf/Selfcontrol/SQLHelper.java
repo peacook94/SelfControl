04eac2de885ecf109573c6c6968cbf0ac6235426
Reinhard <reinhard1994@aol.com>
2015-01-27 21:05:21 +0100

    SQLite Datenbank im entstehen für die Laufzeiten aller Apps, welche dann abgefragt werden können.

---
+++ SelfControl/src/de/dresden/es/inf/Selfcontrol/SQLHelper.java
@@ -0,0 +1,24 @@
+package de.dresden.es.inf.Selfcontrol;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SQLHelper extends SQLiteOpenHelper{
+	
+	public SQLHelper(){
+		super();
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
