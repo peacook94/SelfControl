e46314ae18366e769f46f60a58e86bc4df1a223e
Reinhard <reinhard1994@aol.com>
2015-01-28 14:14:19 +0100

    Datenbank addApp(...) -methode fertig

--- SelfControl/src/de/dresden/es/inf/Selfcontrol/SQLHelper.java
+++ SelfControl/src/de/dresden/es/inf/Selfcontrol/SQLHelper.java
@@ -1,8 +1,10 @@
 package de.dresden.es.inf.Selfcontrol;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import java.util.Date;
 import java.util.Map;
@@ -11,6 +13,17 @@ public class SQLHelper extends SQLiteOpenHelper{
 	
 	private static final int DATABASE_VERSION = 1;
 	private static final String DATABASE_NAME = "Apps";
+	
+	//Table-name
+	private static final String TABLE_NAME = "apps";
+	
+	//Apps-Table Columns name
+	private static final String KEY_ID = "id";
+	private static final String KEY_DATE ="date";
+	private static final String KEY_LABEL ="label";
+	private static final String KEY_RUNTIME ="runtime";
+	
+	private static final String[] COLUMNS = {KEY_ID, KEY_DATE, KEY_LABEL, KEY_RUNTIME};
 	
 	public SQLHelper(Context context){
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -39,8 +52,23 @@ public class SQLHelper extends SQLiteOpenHelper{
 		
 	}
 	
-	public void addApps(String appName, long runtime){
+	public void addApp(Date date,String appName, long runtime){
+		Log.d("adding App: ", appName);
 		
+		// 1. get reference to writable DB
+        SQLiteDatabase db = this.getWritableDatabase();
+        
+        // 2. create ContentValues to add key "column"/value
+        ContentValues values = new ContentValues();
+        values.put(KEY_DATE, date.toString());
+        values.put(KEY_LABEL, appName);
+        values.put(KEY_RUNTIME, String.valueOf(runtime));
+        
+        //3. Insert
+        db.insert(TABLE_NAME, null, values);
+        
+        //4. close
+        db.close();
 	}
 	
 //	public long getRuntime(int id){
@@ -50,5 +78,13 @@ public class SQLHelper extends SQLiteOpenHelper{
 //	public Map<Date, Long> getAllRuntimes(String AppName){
 //		
 //	}
+	
+	public void update(String appName){
+		
+	}
+	
+	public void delete(String appName){
+		
+	}
 
 }
