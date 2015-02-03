f2ceb35ababe38d5aa40022365722bf31c0ec5e1
Asus <Asus@chaos.mshome.net>
2015-02-03 22:32:46 +0100

    umschreiben sekAktivity Grapherstellung; hoffentlich werden jetzt alle 3 Graphen zusammen angezeigt

--- SelfControl/src/de/dresden/es/inf/Selfcontrol/SekActivity.java
+++ SelfControl/src/de/dresden/es/inf/Selfcontrol/SekActivity.java
@@ -2,6 +2,7 @@ package de.dresden.es.inf.Selfcontrol;
 
 import java.text.ParseException;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -51,21 +52,92 @@ public class SekActivity extends Activity{
 //	    }
 //	}
 	
-//	public void drawGraph()
+	public void drawGraph()
+{
+		float t_PHONEUNLOCKED[]=new float[24];
+		float t_BROWSER[]=new float[24];
+		float t_HANGOUTS[]=new float[24];
+		for(int loopcounter=0;loopcounter<24;loopcounter++)
+		{
+			t_PHONEUNLOCKED[loopcounter]=0;
+			t_BROWSER[loopcounter]=0;
+			t_HANGOUTS[loopcounter]=0;
+		}
+		AppsDataSource tmpAppsDataSource = new AppsDataSource(this);
+		tmpAppsDataSource.open();
+		Map<Date, AppId> myMap = new HashMap<Date, AppId>();
+		for(int lc=0;lc<3;lc++)
+		{
+			AppId tmpAppId = AppId.parseString(lc);
+			try {
+				Map<Date, AppId> tmpMap=tmpAppsDataSource.getAppWithDates(tmpAppId);
+				myMap.putAll(tmpMap);
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		tmpAppsDataSource.close();
+		Iterator<Date> iter = myMap.keySet().iterator();
+		
+		while(iter.hasNext())
+		{
+			
+			Date tmpDate =iter.next();
+			AppId myAppId = myMap.get(tmpDate);
+			int dateHour = tmpDate.getHours();
+			if(tmpDate.getDay()==new Date().getDay())
+			{
+				switch(myAppId)
+				{
+				case PHONEUNLOCKED:
+					t_PHONEUNLOCKED[dateHour]+=5*sec;
+					break;
+				case BROWSER:
+					t_BROWSER[dateHour]+=5*sec;
+					break;
+				case HANGOUTS:
+					t_HANGOUTS[dateHour]+=5*sec;
+					break;
+				}
+			}
+		}
+		GraphView gView = (GraphView) findViewById(R.id.sekGraph);
+		
+		DataPoint[] data_PHONEUNLOCKED = new DataPoint[24];
+		DataPoint[] data_BROWSER = new DataPoint[24];
+		DataPoint[] data_HANGOUTS = new DataPoint[24];
+		DataPoint[] data = new DataPoint[24];
+		
+		for(int i=0; i<24;i++){
+			data_PHONEUNLOCKED[i] = new DataPoint(i, t_PHONEUNLOCKED[i]);
+			data_BROWSER[i] = new DataPoint(i, t_BROWSER[i]);
+			data_HANGOUTS[i] = new DataPoint(i, t_HANGOUTS[i]);
+		}
+		
+		LineGraphSeries<DataPoint> series_PHONEUNLOCKED = new LineGraphSeries<DataPoint>(data_PHONEUNLOCKED);
+		LineGraphSeries<DataPoint> series_BROWSER = new LineGraphSeries<DataPoint>(data_PHONEUNLOCKED);
+		LineGraphSeries<DataPoint> series_HANGOUTS = new LineGraphSeries<DataPoint>(data_PHONEUNLOCKED);
+		
+		series_PHONEUNLOCKED.setTitle("Gesamtzeit");
+		series_BROWSER.setTitle("Browser");
+		series_HANGOUTS.setTitle("HANGOUTS");
+		
+		gView.getGridLabelRenderer().setHorizontalAxisTitle("min/h");
+		gView.getGridLabelRenderer().setVerticalAxisTitle("time");
+		gView.addSeries(series_PHONEUNLOCKED);
+		gView.getSecondScale().addSeries(series_BROWSER);
+		gView.getSecondScale().addSeries(series_HANGOUTS);
+	}
+
 	public void drawGraph(Map<Date, AppId> myMap, AppId myAppId)
 	{
 		
-		//die zeit im Minuten!
-//		float t_PHONEUNLOCKED[]=new float[24];
-//		float t_BROWSER[]=new float[24];
-//		float t_HANGOUTS[]=new float[24];
+
 		float time[]=new float[24];
 		for(int loopcounter=0;loopcounter<24;loopcounter++)
 		{
 			time[loopcounter]=0;
-//			t_PHONEUNLOCKED[loopcounter]=0;
-//			t_BROWSER[loopcounter]=0;
-//			t_HANGOUTS[loopcounter]=0;
 		}
 		Iterator<Date> iter = myMap.keySet().iterator();
 		while(iter.hasNext())
@@ -76,45 +148,17 @@ public class SekActivity extends Activity{
 			{
 				time[dateHour]+=5*sec;
 			}
-			AppId id = myMap.get(tmpDate);
-//			switch(id){
-//			case PHONEUNLOCKED:
-//				t_PHONEUNLOCKED[dateHour]+=5*sec;
-//				break;
-//			case BROWSER:
-//				t_BROWSER[dateHour]+=5*sec;
-//				break;
-//			case HANGOUTS:
-//				t_HANGOUTS[dateHour]+=5*sec;
-//				break;
-//			}
 		}
 		GraphView gView = (GraphView) findViewById(R.id.sekGraph);
 		
-//		BarGraphSeries<DataPoint> series = new BarGraphSeries<DataPoint>( new DataPoint[]{
-//				new DataPoint(1, 2),
-//				
-//				new DataPoint(2, 4),
-//		
-//				
-//		}); 
-		
-//		DataPoint[] data_PHONEUNLOCKED = new DataPoint[24];
-//		DataPoint[] data_BROWSER = new DataPoint[24];
-//		DataPoint[] data_HANGOUTS = new DataPoint[24];
 		DataPoint[] data = new DataPoint[24];
 		
 		for(int i=0; i<24;i++){
 			data[i] = new DataPoint(i, time[i]);
-//			data_PHONEUNLOCKED[i] = new DataPoint(i, t_PHONEUNLOCKED[i]);
-//			data_BROWSER[i] = new DataPoint(i, t_BROWSER[i]);
-//			data_HANGOUTS[i] = new DataPoint(i, t_HANGOUTS[i]);
+
 		}
 		
 		LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(data);
-//		LineGraphSeries<DataPoint> series_PHONEUNLOCKED = new LineGraphSeries<DataPoint>(data_PHONEUNLOCKED);
-//		LineGraphSeries<DataPoint> series_BROWSER = new LineGraphSeries<DataPoint>(data_PHONEUNLOCKED);
-//		LineGraphSeries<DataPoint> series_HANGOUTS = new LineGraphSeries<DataPoint>(data_PHONEUNLOCKED);
 		String seriesTitle;
 		switch (myAppId)
 		{
@@ -145,9 +189,6 @@ public class SekActivity extends Activity{
 		
 		gView.getGridLabelRenderer().setHorizontalAxisTitle("min/h");
 		gView.getGridLabelRenderer().setVerticalAxisTitle("time");
-//		gView.addSeries(series_PHONEUNLOCKED);
-//		gView.addSeries(series_BROWSER);
-//		gView.addSeries(series_HANGOUTS);
 	}
 	
 	@Override
@@ -180,25 +221,21 @@ public class SekActivity extends Activity{
 //			});
 //		}
 		
-		
-		/*---------->drawGraph<--------------*/
-		//was muss ich hier einfügen?!?!??!
-		//drawGraph(5);
-//		drawGraph();
-		AppsDataSource tmpAppsDataSource = new AppsDataSource(this);
-		tmpAppsDataSource.open();
-		for(int lc=0;lc<3;lc++)
-		{
-			AppId tmpAppId = AppId.parseString(lc);
-			try {
-				Map<Date, AppId> tmpMap=tmpAppsDataSource.getAppWithDates(tmpAppId);
-				drawGraph(tmpMap,tmpAppId);
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		tmpAppsDataSource.close();
+		drawGraph();
+//		AppsDataSource tmpAppsDataSource = new AppsDataSource(this);
+//		tmpAppsDataSource.open();
+//		for(int lc=0;lc<3;lc++)
+//		{
+//			AppId tmpAppId = AppId.parseString(lc);
+//			try {
+//				Map<Date, AppId> tmpMap=tmpAppsDataSource.getAppWithDates(tmpAppId);
+//				drawGraph(tmpMap,tmpAppId);
+//			} catch (ParseException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		}
+//		tmpAppsDataSource.close();
 		
 	}
 
